6365397a6785a3244b023acd9fb2683b8c3943ef
Dize dev <axiozweb2022@gmail.com>
2024-06-12 22:52:09 -0500

    se agrego estilso home de la landing page asi como los botones de selecionar ell color 😊

--- js/app.js
+++ js/app.js
@@ -1,14 +1,12 @@
 
 const showMenu = (togleId, navId) => {
+
     const toggle = document.getElementById(togleId);
     const nav = document.getElementById(navId);
 
-    if (toggle && nav) {
-
-        toggle.addEventListener('click', () => {
-            nav.classList.toggle('show');
-        });
-    };
+    if (toggle && nav)
+        //agregamos y quitamos la clase al precionar en el boton 
+        toggle.addEventListener('click', () => nav.classList.toggle('show'));
 };
 
 showMenu('nav-toggle', 'nav-menu');
